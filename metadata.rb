9f501ef08703f086999248ef45dbf8ebac7922bc
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2014-02-28 18:00:20 +0100

    0.2.1 version Bump (dev)

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Install a sinopia NPM server (cache & private repo)
 See : https://github.com/rlidwka/sinopia/ '
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 supports 'ubuntu', '>= 12.04' # only tested with precise64
 
