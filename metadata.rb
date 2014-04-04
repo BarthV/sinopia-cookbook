c9b8dbac90ebd59a09b015bc3d0a9273f48059fb
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2014-04-04 10:44:18 +0200

    dependencies update

--- metadata.rb
+++ metadata.rb
@@ -5,12 +5,12 @@ license          'Apache 2.0'
 description      'Install a sinopia NPM server (cache & private repo)
 See : https://github.com/rlidwka/sinopia/ '
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 supports 'ubuntu', '>= 12.04' # only tested with precise64
 
-depends 'apt', '~> 2.3.0'
+depends 'apt', '~> 2.3.8'
 depends 'nodejs', '~> 1.3.0'
-depends 'node', '~> 1.0.1'
+depends 'node', '~> 1.1.0'
 depends 'user', '~> 0.3.0'
-depends 'logrotate', '~> 1.4.0'
+depends 'logrotate', '~> 1.5.0'
