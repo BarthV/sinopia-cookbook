62df63e7ed68fe505c35d8af492ac5aad5c44fe0
Barthelemy Vessemont <bvessemont@gmail.com>
2014-08-07 10:21:21 +0200

    we are now using nodeJS CK v2 :) yeah

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,11 @@ license 'Apache 2.0'
 description 'Install a sinopia NPM server (cache & private repo)
 See : https://github.com/rlidwka/sinopia/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.7'
+version '0.2.8'
 
 supports 'ubuntu', '>= 12.04'
 
-depends 'apt', '~> 2.4.0'
-depends 'nodejs', '~> 1.3.0'
-depends 'user', '~> 0.3.0'
-depends 'logrotate', '~> 1.5.0'
+depends 'apt'
+depends 'nodejs', '~> 2.0'
+depends 'user'
+depends 'logrotate'
