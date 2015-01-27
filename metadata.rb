d7fa7838a469bb2a385b4877e8b8a5017380690a
Barthelemy Vessemont <bvessemont@gmail.com>
2015-01-27 12:02:39 +0100

    0.3.0 : redhat based OS compatibility

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,12 @@ license 'Apache 2.0'
 description 'Install a sinopia NPM server (cache & private repo)
 See : https://github.com/rlidwka/sinopia/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.9'
+version '0.3.0'
 
 supports 'ubuntu', '>= 12.04'
+supports 'redhat'
+supports 'centos'
+supports 'fedora'
 
 depends 'apt'
 depends 'nodejs', '~> 2.0'
